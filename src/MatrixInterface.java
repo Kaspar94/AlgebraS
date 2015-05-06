39b6fa62d34a9079040fcb5c2fd7a76e58c7adaa
joosep41 <joosep41@email.com>
2015-05-06 19:18:51 +0300

    Update MatrixInterface.java

--- src/MatrixInterface.java
+++ src/MatrixInterface.java
@@ -23,11 +23,13 @@ public class MatrixInterface extends Application {
 	private ArrayList<ArrayList<Double>> listA;
 	private ArrayList<ArrayList<Double>> listB;
 	private TextField rowCountA, colCountA, rowCountB, colCountB;
+	private TextField vastusA, vastusB;
+	static int errorCounter = 0;
 
 	public void start(Stage primaryStage) {
 
-		root = new HBox();
-		matA = new VBox();
+		root = new HBox(); //Paneb kaks mati üksteise kõrvale (matA ja matB)
+		matA = new VBox(); //Selle sees pannakse asjad üksteise alla
 		HBox rowA = this.hboxA();
 		VBox actionsA = this.vboxA();
 		matA.getChildren().add(rowA);
@@ -51,41 +53,46 @@ public class MatrixInterface extends Application {
 	private HBox hboxA() {
 
 		HBox row = new HBox();
-		row.setSpacing(20);
+		row.setSpacing(20); // määrab vahed Size, kastide ja update nupu vahel
 		row.setPadding(new Insets(15, 12, 15, 12));
 		Text size = new Text("Size: ");
-		TextField rowCount = new TextField();
-		TextField colCount = new TextField();
-		rowCount.setPrefSize(30, 10);
+		TextField rowCount = new TextField(); //kast ridade arvu jaoks
+		TextField colCount = new TextField(); //kast veergude arvu jaoks
+		rowCount.setPrefSize(30, 10); //mõõtmed
 		colCount.setPrefSize(30, 10);
 		Button update = new Button("Update A");
-		update.setPrefSize(100, 20);
+		update.setPrefSize(100, 20); //nupu mõõtmed
 
-		row.getChildren().addAll(size, rowCount, colCount, update);
+		row.getChildren().addAll(size, rowCount, colCount, update); //lisab HBoxi sulgudes olevad asjad, horisontaalselt üksteise kõrvale
 
-		update.setOnMouseClicked(new EventHandler<MouseEvent>() {
-			public void handle(MouseEvent e) {
+		update.setOnMouseClicked(new EventHandler<MouseEvent>() { //hiiresündmus käsitlemaks update nupule vajutust
+			public void handle(MouseEvent e) { 
 				try {
 					// saame k'tte soovitutd mootmed
-					int r = Integer.parseInt(rowCount.getText());
-					int c = Integer.parseInt(colCount.getText());
+					int r = Integer.parseInt(rowCount.getText()); //saab kastist rowCount kätte soovitud ridade arvu
+					int c = Integer.parseInt(colCount.getText()); // saab kastist colCount kätte soovitud veergude arvu
 
-					colsA = c;
+					colsA = c; //väärtustab isendivälja kasutaja valitud veergude arvuga
+					
 					// kustutame vanad teksfieldid
 					elementsA = new ArrayList<TextField>();
+					
 					if (matA.getChildren().size() == 3) {
+						System.out.println(matA.getChildren().get(1));
 						matA.getChildren().remove(1);
 					}
 					matA.getChildren()
-							.add(1, setMatrixDisplay(r, c, elementsA));
+							.add(1, setMatrixDisplay(r, c, elementsA, "A")); //väärtustab isendiväljad
 
 				} catch (NumberFormatException n) { // kui proovitakse
-													// skeemitada
-					new Error("Rea ja veeru suurus peavad olema arv.");
+					if (errorCounter == 0) {								// skeemitada
+						errorCounter++;
+						new Error("Rea ja veeru suurus peavad olema arv.");
+					}
 				}
 			}
 		});
-		return row;
+		return row; //tagastabki HBoxi
 	}
 
 	private HBox hboxB() {
@@ -117,24 +124,26 @@ public class MatrixInterface extends Application {
 						matB.getChildren().remove(1);
 					}
 					matB.getChildren()
-							.add(1, setMatrixDisplay(r, c, elementsB));
+							.add(1, setMatrixDisplay(r, c, elementsB, "B"));
 
 				} catch (NumberFormatException n) { // kui proovitakse
 													// skeemitada
+					errorCounter = 0;
 					new Error("Rea ja veeru suurus peavad olema arv.");
+					
 				}
 			}
 		});
 		return row;
 	}
 
-	private VBox setMatrixDisplay(int r, int c, ArrayList<TextField> elements) { // rows,
+	private VBox setMatrixDisplay(int r, int c, ArrayList<TextField> elements, String s) { // rows,
 																					// cols,
 		// loome uued textfieldid
 		elements.clear(); // puhastame et uued lisada
 		VBox vb = new VBox(); // hakkab hoidma ridu
-		Text matA = new Text("Maatriks A");
-		vb.getChildren().add(matA);
+		Text mat = new Text("Maatriks " + s);
+		vb.getChildren().add(mat);
 		vb.setPadding(new Insets(15, 12, 15, 12));
 		for (int i = 0; i < r; i++) {
 			HBox rida = new HBox(); // igale reale hbox
@@ -153,15 +162,21 @@ public class MatrixInterface extends Application {
 	private VBox vboxB() {
 		VBox vb = new VBox();
 		HBox multiplication = new HBox();
+		HBox determinant = new HBox();
 		Button puhasta = new Button("Puhasta");
 		Button multiply = new Button("Korruta l2bi arvuga");
 		TextField multiplier = new TextField();
+		multiplier.setPrefSize(50, 20);
 		multiplication.getChildren().addAll(multiply, multiplier);
 		Button liida_b = new Button("Liida maatriks A");
 		Button transponeeri = new Button("Transponeeri"); // transp
 		Button korruta_b = new Button("Korruta maatriksiga A");
+		Button det_b = new Button("Arvuta determinant");
+		vastusB = new TextField();
+		vastusB.setPrefSize(50, 20);
+		determinant.getChildren().addAll(det_b,vastusB);
 		vb.getChildren().addAll(puhasta, multiplication, liida_b, transponeeri,
-				korruta_b);
+				korruta_b, determinant);
 
 		multiply.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent e) {
@@ -172,6 +187,7 @@ public class MatrixInterface extends Application {
 		puhasta.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent e) {
 				clear_textField(elementsB);
+				vastusB.clear();
 			}
 		});
 
@@ -179,6 +195,13 @@ public class MatrixInterface extends Application {
 			public void handle(MouseEvent e) {
 				listB = textFieldToList(elementsB, colsB);
 				transponeerija(listB, matB, elementsB);
+			}
+		});
+		
+		det_b.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent e) {
+				listB = textFieldToList(elementsB, colsB);
+				detArvutaja(listB,vastusB);
 			}
 		});
 
@@ -189,15 +212,21 @@ public class MatrixInterface extends Application {
 	private VBox vboxA() {
 		VBox vb = new VBox();
 		HBox multiplication = new HBox();
+		HBox determinant = new HBox();
 		Button puhasta = new Button("Puhasta");
 		Button multiply = new Button("Korruta l2bi arvuga");
 		TextField multiplier = new TextField();
+		multiplier.setPrefSize(50, 20);
 		multiplication.getChildren().addAll(multiply, multiplier);
 		Button liida_b = new Button("Liida maatriks B");
 		Button transponeeri = new Button("Transponeeri"); // transp
 		Button korruta_b = new Button("Korruta maatriksiga B");
+		Button det_a = new Button("Arvuta determinant");
+		vastusA = new TextField();
+		vastusA.setPrefSize(50, 20);
+		determinant.getChildren().addAll(det_a,vastusA);
 		vb.getChildren().addAll(puhasta, multiplication, liida_b, transponeeri,
-				korruta_b);
+				korruta_b, determinant);
 
 		multiply.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent e) {
@@ -208,6 +237,7 @@ public class MatrixInterface extends Application {
 		puhasta.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent e) {
 				clear_textField(elementsA);
+				vastusA.clear();
 			}
 		});
 
@@ -236,11 +266,21 @@ public class MatrixInterface extends Application {
 					b.print();
 					listToTextField(b.getList(), elementsA);
 				} catch(Exception m) {
-					new Error(m.getMessage());
+					if (errorCounter == 0) {
+						errorCounter++;
+						new Error(m.getMessage());
+					}
 				}
 			}
 		});
-
+		
+		det_a.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent e) {
+				listA = textFieldToList(elementsA, colsA);
+				detArvutaja(listA, vastusA);
+			}
+		});
+		
 		return vb;
 
 	}
@@ -252,7 +292,10 @@ public class MatrixInterface extends Application {
 			a.liida(new Matrix(listB));
 			this.listToTextField(a.getList(), elements);
 		} catch (Exception e) {
-			new Error(e.getMessage());
+			if (errorCounter == 0) {
+				errorCounter++;
+				new Error(e.getMessage());
+			}
 		}
 	}
 
@@ -263,24 +306,39 @@ public class MatrixInterface extends Application {
 				t.setText(Double.toString((Double.parseDouble(t.getText()) * korrutaja)));
 			}
 		} catch (NumberFormatException n) {
-			new Error("Korrutaja ning elemendid maatriksis peavad olema arvud");
+			if (errorCounter == 0) {
+				errorCounter++;
+				new Error("Korrutaja ning elemendid maatriksis peavad olema arvud");
+			}
 		}
 	}
 
 	private void clear_textField(ArrayList<TextField> elements) {
-		for (TextField t : elements) {
-			t.clear();
+		try {
+			for (TextField t : elements) {
+				t.clear();
+			}
+		} catch (NullPointerException e) {
+			new Error("Pole midagi puhastada.");
 		}
 	}
 
 	private void transponeerija(ArrayList<ArrayList<Double>> list, VBox vb,
 			ArrayList<TextField> elements) {
-		Matrix a = new Matrix(list);
-		a.transponeeri();
-		vb.getChildren().remove(1);
-		vb.getChildren().add(1,
-				(setMatrixDisplay(a.getRows(), a.getCols(), elements)));
-		this.listToTextField(a.getList(), elements);
+		if (!list.isEmpty()) { 
+			Matrix a = new Matrix(list);
+			a.transponeeri();
+			vb.getChildren().remove(1);
+			vb.getChildren().add(1,
+				(setMatrixDisplay(a.getRows(), a.getCols(), elements,"")));
+			this.listToTextField(a.getList(), elements);
+		}
+		else {
+			if (errorCounter == 0) {
+				errorCounter++;
+				new Error("Andmeid pole sisestatud");
+			}
+		}
 	}
 
 	private void listToTextField(ArrayList<ArrayList<Double>> list,
@@ -290,6 +348,19 @@ public class MatrixInterface extends Application {
 			for (Double d : innerRow) {
 				elements.get(c).setText(Double.toString(d));
 				c += 1;
+			}
+		}
+	}
+	
+	private void detArvutaja(ArrayList<ArrayList<Double>> list, TextField vastus) {
+		try {
+			Determinant a = new Determinant(list);
+			double result = a.calculate_det();
+			vastus.setText(Double.toString(result));
+		} catch (IndexOutOfBoundsException i) {
+			if (errorCounter == 0) {
+				errorCounter++;
+				new Error("Maatriks on vigane.");
 			}
 		}
 	}
@@ -318,7 +389,10 @@ public class MatrixInterface extends Application {
 				}
 			}
 		} catch (NumberFormatException e) {
-			new Error("Maatriks tohib sisaldada aind numbreid. Proovi uuesti.");
+			if (errorCounter == 0) {
+				errorCounter++;
+				new Error("Maatriks tohib sisaldada aind numbreid. Proovi uuesti.");
+			}
 		} finally {
 			return list;
 		}
