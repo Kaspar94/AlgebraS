a060eeab54be830944dd89472ff753051003e14a
Kaspar <kaspar94@tdl.ee>
2015-05-16 20:43:54 +0300

    Update Error.java

--- src/Error.java
+++ src/Error.java
@@ -7,10 +7,24 @@ import javafx.stage.Stage;
 
 public class Error extends Application {
 	private String err;
-	public Error(String s) {
+	private int count;
+
+	public Error() {
+		count = 0;
+	};
+	
+	public void newError(String s) {
+		if (count != 0)
+			return;
 		this.err = s;
 		this.start(new Stage());
+		count += 1;
 	}
+	
+	public void resetCount() {
+		count = 0;
+	}
+
 	public void start(Stage p) {
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Veateade");
